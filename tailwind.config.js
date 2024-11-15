df45e0c27e3a4c488ff21c018903614b1ab5353f
Franco-luque <francofjlh@gmail.com>
2024-11-15 09:14:12 -0300

    configura paleta de colores en tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sparkRed: {
+          600: "rgb(242, 95, 58)",
+          200: "rgb(255, 239, 234)",
+        },
+        darkBlue: {
+          600: "rgb(35, 44, 81)",
+          900: "rgb(29, 30, 37)",
+        },
+        darkGrayBlue: "rgb(143, 148, 167)",
+        lightGrey: "rgb(249, 249, 249)",
+      },
+    },
   },
   plugins: [],
 };
